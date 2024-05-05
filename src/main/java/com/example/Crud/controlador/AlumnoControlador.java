6a944142bc8a3755f65995fcc9f325240ee3f92f
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-05-05 11:00:04 -0500

    first commit

---
+++ src/main/java/com/example/Crud/controlador/AlumnoControlador.java
@@ -0,0 +1,43 @@
+package com.example.Crud.controlador;
+
+import org.springframework.http.ResponseEntity;
+import com.example.Crud.alumnoServicio.AlumnoServicio;
+import com.example.Crud.modelo.Alumno;
+import com.example.Crud.repositorio.AlumnoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class AlumnoControlador {
+
+    @Autowired
+    private AlumnoRepositorio repositorio;
+    @Autowired
+    private AlumnoServicio estudianteService;
+
+    @GetMapping("/Alumno")
+    public List<Alumno> ListaAlumno(){
+        return repositorio.findAll();
+    }
+    
+    @PostMapping("/Alumno")
+    public Alumno RegistroEstudiante(@RequestBody Alumno alumno){
+        return repositorio.save(alumno);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Alumno> actualizarEstudiante(@PathVariable Long id, @RequestBody Alumno estudianteActualizado) {
+        Alumno estudianteActualizadoResultado = estudianteService.actualizarEstudiante(id, estudianteActualizado);
+        return ResponseEntity.ok(estudianteActualizadoResultado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> eliminarEstudiante(@PathVariable Long id) {
+        repositorio.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/example/Crud/repositorio/AlumnoRepositorio.java
@@ -0,0 +1,11 @@
+package com.example.Crud.repositorio;
+
+import com.example.Crud.modelo.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepositorio extends JpaRepository<Alumno, Long> {
+    //Alumno findByCorreo(String email);
+
+}
