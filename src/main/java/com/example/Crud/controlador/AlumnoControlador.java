681808f4e5cd446bbf09c83fe5cfe77feeb9008e
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-05-12 02:06:07 -0500

    Angular03

--- src/main/java/com/example/Crud/controlador/AlumnoControlador.java
+++ src/main/java/com/example/Crud/controlador/AlumnoControlador.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/")
 @CrossOrigin(origins = "https://crud-420e9.web.app")
 
 public class AlumnoControlador {
