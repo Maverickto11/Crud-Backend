a0477c6e18f1167bcd3d042092e0812b882c8e11
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-05-16 04:34:28 -0500

    Cors05

--- src/main/java/com/example/Crud/CorsConfig.java
+++ src/main/java/com/example/Crud/CorsConfig.java
@@ -3,23 +3,18 @@ package com.example.Crud;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1")
-                        .allowedOrigins("https://crud-estudiantes-d1a57.web.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
             }
-        };
-    }
 }
 
