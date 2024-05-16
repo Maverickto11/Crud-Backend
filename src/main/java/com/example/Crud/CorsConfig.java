0710a07fb5d6f0d961779e3599fb8902ae321099
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-05-16 03:40:11 -0500

    Cors03

--- src/main/java/com/example/Crud/CorsConfig.java
+++ src/main/java/com/example/Crud/CorsConfig.java
@@ -13,14 +13,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://crud-estudiantes-d1a57.web.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins("https://crud-estudiantes-d1a57.web.app").allowedMethods("*");
             }
         };
     }
 }
-
 
