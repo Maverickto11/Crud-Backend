b62bdee98062396104e968c686adefd80eb45930
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-05-19 04:14:27 -0500

    Cors09

--- src/main/java/com/example/Crud/CorsConfig.java
+++ src/main/java/com/example/Crud/CorsConfig.java
@@ -12,11 +12,26 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class CorsConfig{
-
+public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://crud-estudiantes-d1a57.web.app") // Cambia a tu dominio de Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
 
